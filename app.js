3aa127c7a698613b768a4ad8041e4b7fd03968e0
Anis-Dhaoui <freeconcept6@gmail.com>
2022-10-21 21:28:06 +0100

    Establish connection with DB

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const db = require('./models');
+db.sequelize.sync().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
