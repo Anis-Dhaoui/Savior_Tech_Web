b780af22869ee0d7d17ea0414ff9b9b30b2b63dd
Anis Dhaoui <freeconcept6@gmail.com>
2022-10-22 17:09:17 +0100

    Adding event endpoint to app.js

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var eventRouter = require('./routes/eventsRouter')
 
 var app = express();
 
@@ -26,8 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/events', eventRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
