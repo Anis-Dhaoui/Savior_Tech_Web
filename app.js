613d2bb46998292dbd78814aa692993f96cd19a7
Somrani1994 <somrani1994@gmail.com>
2022-10-26 21:03:00 +0100

    upload image

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var fileUpload = require('express-fileupload');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require("body-parser");
 require("dotenv").config();
 
 
@@ -36,8 +38,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload());
 app.use('/events', eventRouter);
 
 app.use('/users', userRouter);
--- routes/Question.js
+++ routes/Question.js
@@ -2,15 +2,45 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 
-
 router.post('/add', (req, res) => {
-    db.questions.create(req.body).then(
-        (p) => {
-            res.send(p);
-        }
-    );
-});
+    if (!req.files) {
+        db.questions.create({
+            description: req.body.description,
+            date: req.body.date,
+            titre: req.body.titre,
+            UserId: req.body.UserId
+        }).then(
+            (p) => {
+                res.send(p);
+            }
+        );
+    } else {
 
+        var file = req.files.image;
+        var img_name = file.name;
+
+        if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+
+            file.mv('public/images/upload/' + img_name, function(err) {
+
+                {
+                    db.questions.create({
+                        description: req.body.description,
+                        date: req.body.date,
+                        titre: req.body.titre,
+                        image: img_name,
+                        UserId: req.body.UserId
+                    }).then(
+                        (p) => {
+                            res.send(p);
+                        }
+                    );
+                }
+            });
+        }
+
+    }
+});
 
 router.get('/', function(req, res, next) {
     db.questions.findAll().then((resp) => {
