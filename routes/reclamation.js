7f8eb58b4379a3f484006bcedb732a71e74aa3f1
Somrani1994 <somrani1994@gmail.com>
2022-11-04 22:03:28 +0100

    update final

--- routes/reclamation.js
+++ routes/reclamation.js
@@ -12,8 +12,8 @@ const { userByUsername } = require("../controllers/user");
 
 const router = express.Router();
 
-router.post("/reclamation/add/:userName", requireSignin, addReclamation);
-router.get("/reclamation/get/:userName", requireSignin, reclamationsByUser);
+router.post("/reclamation/add/:userName",  addReclamation);
+router.get("/reclamation/get/:userName", reclamationsByUser);
 router.put("/reclamation/approve/:userName/:reclamationId",requireSignin,traiterReclamation);
 router.get("/reclamation/getall/:userName", requireSignin, getAllReclamations);
 // any route containing username our app will first execute userByUsername()
